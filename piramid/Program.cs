ffbfbeeba13eba8dd8471e21c5598c744bd1951a
muradheydarov <muradheyderov@gmail.com>
2017-08-24 20:05:47 +0400

    initial commit

---
+++ piramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace piramid
+{
+    class Program
+    {
+        static void MakePyramid (int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                for (int m = i; m<=n; m++)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 0; j < 2*i-1; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main()
+        {
+            MakePyramid(6);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pyramid reverse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyramid_reverse
+{
+    class Program
+    {
+        static void ReversePyramid(int n)
+        {
+            for (int i = n; i <= n && i >=0; i--)
+            {
+                for (int m = n; m >= i && m>=0; m--)
+                {
+                    Console.Write("  ");
+                }
+                for (int j = 0; j < 2 * i - 1; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main()
+        {
+            ReversePyramid(6);
+            Console.ReadKey();
+        }
+    }
+
+}
